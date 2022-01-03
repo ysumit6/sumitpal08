c2f98fed01b0e109d53acef9c32c22a97f78e16f
sumitpal08 <Sumit.Pal21@st.niituniversity.in>
2022-01-03 09:17:56 +0000

    Calc Menu Done

---
+++ Calc.java
@@ -0,0 +1,28 @@
+public class Calc
+{ 
+ public static void main(String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 System.out.print("Enter number 1:");
+	 int i =sc.nextInt();
+	 System.out.print("Enter number 2:");
+	 int j=sc.nextInt();
+	 System.out.println("1. Add");
+	 System.out.println("1. Sub");
+	 System.out.println("1. Mul");
+	 System.out.println("1. Div");
+	 System.out.println("Enter Choice:");
+	 int ch=sc.nextInt();
+	 switch(ch)
+		{
+		 case1:// ADD
+			break;
+		 case1:// Sub
+			break;
+		 case1:// Mul
+			break;
+		 case1:// Div
+			break;
+		}
+	}
+}
